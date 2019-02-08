72fda50d048795463254ef11277652b9bdde8e82
Denivy Ruck <denivyruck1@gmail.com>
2019-02-08 15:17:36 +0100

    Improve upload script verbosity

--- ci/upload.sh
+++ ci/upload.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+echo "Starting release job for "
+
 JOB_SCOPE=$(echo $1 | cut -f1 -d' ')
 JOB_TYPE=$(echo $1 | cut -f2 -d' ')
 TARGET_OS=$(echo $1 | cut -f3 -d' ')
+
+echo "$JOB_TYPE"
 
 if [[ "$JOB_TYPE" != "Release" ]]; then
     exit
@@ -16,6 +20,8 @@ DESTINATION_PATH=karaboExtensions/tags/$COMMIT_TAG
 
 pushd $CI_PROJECT_DIR
 
+echo "Creating wheel at $CI_PROJECT_DIR"
+
 # Create wheel in dist/
 python setup.py bdist_wheel
 
@@ -25,10 +31,14 @@ mv /dist/GUI_Extensions* $WHEEL_FILE
 
 # Make directories on remote
 SSH_KARABO_DIR=/var/www/html/karabo
+
+echo "Creating remote directory: $SSH_KARABO_DIR/$DESTINATION_PATH"
 sshpass -p "$XDATA_PWD" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $SSH_USER_HOST "mkdir -p $SSH_KARABO_DIR/$DESTINATION_PATH"
 
 # Copy wheel to correct directory
 SSH_PREFIX=$SSH_USER_HOST:$SSH_KARABO_DIR
+
+echo "Moving $WHEEL_FILE to $SSH_PREFIX/$DESTINATION_PATH"
 sshpass -p "$XDATA_PWD" scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $WHEEL_FILE $SSH_PREFIX/$DESTINATION_PATH
 
 popd
