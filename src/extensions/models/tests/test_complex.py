5182682f49c849ae81bd79ea5e2bc9fcba59c3c8
Cammille Carinan <cammille.carinan@xfel.eu>
2023-11-09 11:43:43 +0100

    Detector Cells: Move models and use initial values for dimension

--- src/extensions/models/tests/test_complex.py
+++ src/extensions/models/tests/test_complex.py
@@ -1,5 +1,8 @@
+import pytest
+
 from extensions.models.api import (
-    LimitedDoubleLineEditModel, VectorLimitedDoubleLineEditModel)
+    DetectorCellsModel, LimitedDoubleLineEditModel, MultipleDetectorCellsModel,
+    VectorLimitedDoubleLineEditModel)
 from karabo.common.scenemodel.tests.utils import (
     base_widget_traits, single_model_round_trip)
 
@@ -24,3 +27,18 @@ def test_limited_double_line_edit():
     read_model = single_model_round_trip(model)
     assert read_model.decimals == 2
     assert read_model.parent_component == model.parent_component
+
+
+@pytest.mark.parametrize("model_cls",
+                         [DetectorCellsModel, MultipleDetectorCellsModel])
+def test_detector_cells_widget(model_cls):
+    traits = base_widget_traits()
+    traits['rows'] = 40
+    traits['columns'] = 20
+    traits['legend_location'] = 'right'
+    model = model_cls(**traits)
+
+    read_model = single_model_round_trip(model)
+    assert read_model.rows == 40
+    assert read_model.columns == 20
+    assert read_model.legend_location == 'right'
