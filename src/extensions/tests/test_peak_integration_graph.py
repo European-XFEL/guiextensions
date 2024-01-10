b52fdc7095ce4abe290f72dc30650915f701d7cd
Cammille Carinan <cammille.carinan@xfel.eu>
2024-01-10 10:15:35 +0100

    feat(peakintegrationgraph): use ndarray and show peak markers

--- src/extensions/tests/test_peak_integration_graph.py
+++ src/extensions/tests/test_peak_integration_graph.py
@@ -3,7 +3,9 @@ from numpy.testing import assert_array_equal
 
 from extensions.peak_integration_graph import (
     NUM_PEAKS, DisplayPeakIntegrationGraph)
-from karabo.native import Configurable, Hash, Node, VectorInt32, VectorUInt32
+from extensions.utils import get_ndarray_hash_from_data
+from karabo.native import (
+    Configurable, Hash, NDArray, Node, UInt16, VectorInt32, VectorUInt32)
 from karabogui.testing import (
     GuiTestCase, get_class_property_proxy, set_proxy_hash)
 
@@ -22,19 +24,25 @@ class PeakIntegrationNode(Configurable):
 
 class Device(Configurable):
     node = Node(PeakIntegrationNode)
+    trace = NDArray(
+        dtype=UInt16,
+        shape=(40,)
+    )
 
 
 class TestPeakIntegrationGraph(GuiTestCase):
     def setUp(self):
         super(TestPeakIntegrationGraph, self).setUp()
-
         schema = Device.getClassSchema()
-        self.proxy = proxy = get_class_property_proxy(schema, 'node')
 
+        self.peak_proxy = proxy = get_class_property_proxy(schema, 'node')
         set_proxy_hash(proxy, Hash('node', Hash(DEFAULT_VALUES)))
 
         self.controller = DisplayPeakIntegrationGraph(proxy=proxy)
         self.controller.create(None)
+
+        self.trace_proxy = get_class_property_proxy(schema, 'trace')
+        self.controller.visualize_additional_property(self.trace_proxy)
 
     def tearDown(self):
         self.controller.destroy()
@@ -42,13 +50,28 @@ class TestPeakIntegrationGraph(GuiTestCase):
 
     def test_basics(self):
         controller = self.controller
+
+        # Check peak region items
         assert len(controller._peak_regions) == NUM_PEAKS
-        assert len(controller._peak_markers) == NUM_PEAKS
+        assert len(controller._peak_lines) == NUM_PEAKS
         assert len(controller._base_regions) == NUM_PEAKS
 
+        # Check peak region data
         assert_array_equal(controller._peak_positions, np.arange(0, 30, 3))
         assert_array_equal(controller._peak_widths, [-1, 1])
         assert_array_equal(controller._peak_baseline, [-2, -1])
+
+        # Check peak region visibility
+        for item in controller.region_items:
+            assert item.isVisible() is False
+
+        # Check trace and markers
+        assert controller._curve_item.xData is None
+        assert controller._curve_item.yData is None
+
+        peak_x, peak_y = controller._peak_item.getData()
+        assert_array_equal(peak_x, [])
+        assert_array_equal(peak_y, [])
 
     def test_update_position(self):
         # Reset controller monitor
@@ -61,7 +84,7 @@ class TestPeakIntegrationGraph(GuiTestCase):
         # Update node value
         values = DEFAULT_VALUES.copy()
         values['peakPositions'] = np.arange(0, 30, 3) + 2  # shift by 2
-        set_proxy_hash(self.proxy, Hash('node', Hash(values)))
+        set_proxy_hash(self.peak_proxy, Hash('node', Hash(values)))
 
         # Check changes
         assert controller._peak_region_updated is True
@@ -78,7 +101,7 @@ class TestPeakIntegrationGraph(GuiTestCase):
         # Update node value
         values = DEFAULT_VALUES.copy()
         values['peakWidths'] = np.array([-2, 2])
-        set_proxy_hash(self.proxy, Hash('node', Hash(values)))
+        set_proxy_hash(self.peak_proxy, Hash('node', Hash(values)))
 
         # Check changes
         assert controller._peak_region_updated is True
@@ -95,8 +118,37 @@ class TestPeakIntegrationGraph(GuiTestCase):
         # Update node value
         values = DEFAULT_VALUES.copy()
         values['peakBaseline'] = np.array([-3, -2])
-        set_proxy_hash(self.proxy, Hash('node', Hash(values)))
+        set_proxy_hash(self.peak_proxy, Hash('node', Hash(values)))
 
         # Check changes
         assert controller._peak_region_updated is False
         assert controller._base_region_updated is True
+
+    def test_update_trace(self):
+        controller = self.controller
+
+        trace = np.arange(40) * 2
+        set_proxy_hash(self.trace_proxy,
+                       Hash('trace', get_ndarray_hash_from_data(trace)))
+
+        # Check peak region items
+        assert len(controller._peak_regions) == NUM_PEAKS
+        assert len(controller._peak_lines) == NUM_PEAKS
+        assert len(controller._base_regions) == NUM_PEAKS
+
+        # Check peak region data
+        assert_array_equal(controller._peak_positions, np.arange(0, 30, 3))
+        assert_array_equal(controller._peak_widths, [-1, 1])
+        assert_array_equal(controller._peak_baseline, [-2, -1])
+
+        # Check peak region visibility
+        for item in controller.region_items:
+            assert item.isVisible() is True
+
+        # Check trace and markers
+        assert_array_equal(controller._curve_item.xData, np.arange(trace.size))
+        assert_array_equal(controller._curve_item.yData, trace)
+
+        peak_x, peak_y = controller._peak_item.getData()
+        assert_array_equal(peak_x, DEFAULT_VALUES['peakPositions'])
+        assert_array_equal(peak_y, trace[DEFAULT_VALUES['peakPositions']])
